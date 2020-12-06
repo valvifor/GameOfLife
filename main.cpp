5f2275e585195b0f1794a943691eff771b6e8605
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2020-12-06 20:42:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,115 @@
 #include "Parser.h"
 #include "Field.h"
 
-int main(){
-    string command;
-    Field field;
+using namespace std;
 
+TEST(TestSet, test1){
+    Field game;
+    int x = 6;
+    game.set(x, x);
+    EXPECT_EQ('*', game.cellContent(x, x));
+}
+
+TEST(TestSet, test2){
+    Field game;
+    int x = 6;
+    int y = 3;
+    game.set(x, y);
+    EXPECT_EQ('*', game.cellContent(x, y));
+}
+
+TEST(TestClear, test3){
+    Field game;
+    int x = 5;
+    game.set(x, x);
+    game.clear(x, x);
+    EXPECT_EQ('.', game.cellContent(x, x));
+}
+
+TEST(TestClear, test4){
+    Field game;
+    int x = 5;
+    int y = 9;
+    game.set(x, y);
+    game.clear(x, y);
+    EXPECT_EQ('.', game.cellContent(x, y));
+}
+
+TEST(TestStep, test5){
+    Field game;
+    int x = 3;
+    game.set(x, x);
+    game.step();
+    EXPECT_EQ('.', game.cellContent(x, x));
+}
+
+TEST(TestNeighbors, test6){
+    Field game;
+    int x = 1;
+    game.set(x, x);
+    x++;//2
+    game.set(x, x);
+    x++;//3
+    game.set(x, x);
+    game.step();
+    EXPECT_EQ(game.numberOfLiveNeighbors(x, x), 1);
+    x--;//2
+    EXPECT_EQ(game.numberOfLiveNeighbors(x, x), 2);
+}
+
+TEST(TestStep, test7){
+    Field game;
+    int x = 1;
+    game.set(x, x);
+    x++;//2
+    game.set(x, x);
+    x++;//3
+    game.set(x, x);
+    game.step();
+    EXPECT_EQ('.', game.cellContent(x, x));
+    x--;//2
+    EXPECT_EQ('*', game.cellContent(x, x));
+    x--;//1
+    EXPECT_EQ('.', game.cellContent(x, x));
+}
+
+TEST(TestStep, test8){
+    Field game;
+    int x = 6;
+    int y = 7;
+    int a = 6;
+    int b = 8;
+    game.set(x, x);
+    game.set(y, y);
+    game.set(a, b);
+    game.step();
+    EXPECT_EQ('*', game.cellContent(x, y));
+}
+
+TEST(TestReset, test9){
+    Field game;
+    for (int i = 0; i < spaceHeight; i++){
+        game.set(i, i);
+    }
+    game.reset();
+    for (int i = 0; i < spaceHeight; i++){
+        for (int j = 0; j < spaceWidth; j++){
+            EXPECT_EQ('.', game.cellContent(i, i));
+        }
+    }
+}
+
+TEST(LargeTest, test10){
+    Field game;
+    for (int i = 0; i < spaceHeight; i++) {
+        for (int j = 0; j < spaceWidth; j++) {
+            if (i != j)
+                game.set(i, j);
+        }
+    }
+    cout << game;
+    for (int i = 0; i < 1001; i++){
+        game.step();
+    }
+    cout << game;
 }

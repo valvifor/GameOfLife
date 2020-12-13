f3924dfd23dffe039aac44bda849944cf9c74d6b
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2020-12-13 14:20:15 +0700

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -2,14 +2,24 @@
 #define GAMELIFE_PARSER_H
 
 #include "Field.h"
+#include "Command.h"
 
-class Parser{
-private:
-    Field space;
-public:
-    string getCom();
-    void getCommand();
+enum class Commands{
+    reset, set, clear, step, back, save, load, ERROR
 };
 
+class Parser {
+public:
+    Commands getCommand(const string&);
+};
+
+class Game{
+private:
+    Command command;
+    Parser parser;
+public:
+    Game() = default;
+    void game();
+};
 
 #endif //GAMELIFE_PARSER_H
